ea5006759dd47faa4d2452aca7b940bc74cb9785
Ivan Cavalheiro <ivanscavalheiro@gmail.com>
2017-09-03 18:05:45 -0300

    Gulp working for JS and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('sass', () => {
+gulp.task('css', () => {
     return gulp.src('sass/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass())
+    .pipe(sass({includePaths: __dirname + '/sass/'}))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/styles/'));
 });
 
 gulp.task('js', () =>{
-
+    return gulp.src('scripts/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/scripts/'));
 });
 
-gulp.task('dist', ['scss', 'js'], () => {
-    
+gulp.task('dist', ['css', 'js'], () => {
+
 });
